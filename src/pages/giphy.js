a005023348e514bbb53d1c7d1428f68c0ad8b06e
willowmoon <willowmoon@pop-os.localdomain>
2020-06-06 23:22:23 -0400

    pull in movie api with process env variable

---
+++ src/pages/giphy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const giphy = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default giphy
---
+++ src/pages/movies.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react"
+import "../css/styles.css"
+
+export default function Movies() {
+  const [movieData, setMovieData] = useState({})
+  const [movieTitle, setMovieTitle] = useState("")
+  const [movie, setMovie] = useState("")
+
+  useEffect(() => {
+    const apiKey = process.env.MOVIE_KEY
+    const movieUrl = `https://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`
+    const makeApiCall = async () => {
+      const res = await fetch(movieUrl)
+      const json = await res.json()
+      setMovieData(json)
+    }
+    makeApiCall()
+  }, [movieTitle])
+
+  const handleSubmit = async title => {
+    setMovieTitle(title)
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    handleSubmit(movie)
+    setMovie("")
+  }
+
+  const handleChange = e => {
+    const title = e.target.value
+    setMovie(title)
+  }
+
+  return (
+    <div className="App">
+      <h1 className="header">movie api</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="movietitle">
+          <input
+            id="movieTitle"
+            type="text"
+            placeholder="title"
+            value={movie}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <input type="submit" value="search" />
+        </label>
+      </form>
+      <br />
+      {movieData.Response === "False" ? (
+        <></>
+      ) : (
+        <div>
+          <h1 className="big">{movieData.Title}</h1>
+          <h1 className="subhead">{movieData.Year}</h1>
+          <img src={movieData.Poster} alt={movieData.Title} />
+          <p className="subhead">{movieData.Genre}</p>
+          <br />
+          <p>{movieData.Plot}</p>
+        </div>
+      )}
+    </div>
+  )
+}
